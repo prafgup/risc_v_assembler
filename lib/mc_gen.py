9f670057ba79cafd15e7656bce83422e1a8cb9e9
Shubham <shubhamsingh14272000@gmail.com>
2020-03-02 19:11:17 +0530

    Machine Code generator File Error Handling Completed

--- lib/mc_gen.py
+++ lib/mc_gen.py
@@ -36,32 +36,44 @@ def DecodeInstruction(instructionParts, instructionTemplate):
         machineCode = IObj.machineCode(instructionParts[0], int(instructionParts[1]), int(instructionParts[2]), int(instructionParts[3]))
     elif (instructionTemplate[2] == 'S'):
         SObj = S()
+        instructionParts[1] = instructionParts[1][1:]
         instructionParts[2] = instructionParts[2][1:]
-        instructionParts[3] = instructionParts[3][1:]
-        machineCode = SObj.machineCode(instructionParts[0], int(instructionParts[1]), int(instructionParts[2]), int(instructionParts[3]))
+        machineCode = SObj.machineCode(instructionParts[0], int(instructionParts[3]), int(instructionParts[1]), int(instructionParts[2]))
     elif (instructionTemplate[2] == 'UJ'):
         UJObj = UJ()
         instructionParts[1] = instructionParts[1][1:]
         machineCode = UJObj.machineCode(instructionParts[0], int(instructionParts[1]), int(instructionParts[2]))
     elif (instructionTemplate[2] == 'U'):
-        UJObj = UJ()
+        UObj = U()
         instructionParts[2] = instructionParts[2][1:]
-        machineCode = UJObj.machineCode(instructionParts[0], int(instructionParts[1]), int(instructionParts[2]))
+        machineCode = UObj.machineCode(instructionParts[0], int(instructionParts[1]), int(instructionParts[2]))
     elif (instructionTemplate[2] == 'SB'):
-        UJObj = UJ()
-        instructionParts[2] = instructionParts[2][1:]
-        instructionParts[3] = instructionParts[3][1:]
-        machineCode = UJObj.machineCode(instructionParts[0], int(instructionParts[1]), int(instructionParts[2]), int(instructionParts[3]))
+        SBObj = SB()
+        instructionParts[1] = instructionParts[1][1:]       #rs1
+        instructionParts[2] = instructionParts[2][1:]       #rs2
+        machineCode = SBObj.machineCode(instructionParts[0], int(instructionParts[3]), int(instructionParts[1]), int(instructionParts[2]))
     return machineCode
 
 def LookUpInstruction(instructionParts, lineNo):
     for instructionTemplate in InstructionTable:
         if (instructionParts[0] == instructionTemplate[0]):
             if (instructionTemplate[2] == 'R' and len(instructionParts) != 4):
-                print("Error at line number", lineNo,": Expected 4 arguments but got ",len(instructionParts)-1)
+                print("Error at line number", lineNo,": Expected 3 arguments but got",len(instructionParts)-1)
                 return None
             elif (instructionTemplate[2] == 'I' and len(instructionParts) != 4):
-                print("Error at line number", lineNo, ": Expected 4 arguments but got ",len(instructionParts)-1)
+                print("Error at line number", lineNo, ": Expected 3 arguments but got",len(instructionParts)-1)
+                return None
+            elif (instructionTemplate[2] == 'S' and len(instructionParts) != 4):
+                print("Error at line number", lineNo, ": Expected 3 arguments but got",len(instructionParts)-1)
+                return None
+            elif (instructionTemplate[2] == 'UJ' and len(instructionParts) != 3):
+                print("Error at line number", lineNo, ": Expected 2 arguments but got",len(instructionParts)-1)
+                return None
+            elif (instructionTemplate[2] == 'SB' and len(instructionParts) != 4):
+                print("Error at line number", lineNo, ": Expected 3 arguments but got",len(instructionParts)-1)
+                return None
+            elif (instructionTemplate[2] == 'U' and len(instructionParts) != 3):
+                print("Error at line number", lineNo, ": Expected 2 arguments but got",len(instructionParts)-1)
                 return None
             return instructionTemplate
     print ("Instruction at line number", lineNo,"not recognized")           
