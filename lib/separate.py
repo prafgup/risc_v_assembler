32f5ee9a6e3dee1c544a6d5e28db2ce39803526a
shobhit <shobhitgupta907@gmail.com>
2020-03-02 16:56:45 +0530

    Separate test and data files

---
+++ lib/separate.py
@@ -0,0 +1,22 @@
+input_file=open("testWrite.txt",'r')
+data_file=open("data_file.txt",'w')
+text_file=open("text_file.txt",'w')
+
+dataWrite=False
+textWrite=False
+
+for line in input_file.readlines():
+    if dataWrite:
+        data_file.write(line)
+    if textWrite:
+        text_file.write(line)
+    if(line.strip()==".data"):
+        dataWrite=True
+        textWrite=False
+    elif(line.strip()==".text"):
+        dataWrite=False
+        textWrite=True
+
+input_file.close()
+data_file.close()
+text_file.close()
