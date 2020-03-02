b4c9c472971978a9b6d88bae109f4fb3b0e7943d
Praful Gupta <prafulgupta6@gmail.com>
2020-03-02 23:58:15 +0530

    label Dict Creater

--- lib/file_preprocess.py
+++ lib/file_preprocess.py
@@ -27,8 +27,22 @@ class initParser:
 		f = open(name,"w+")
 		for el in li:
 			f.write(el+"\n")
-	
+	def generate_labels_and_list(self,name):
+		f = open(name, "r")
+		lis = f.readlines()
+		dic = {}
+		label_count =0
+		new_lis = []
+		for elem in lis:
+			if ":" in elem:
+				dic[elem.split(":")[0]] = lis.index(elem) - label_count
+				label_count+=1
+			else:
+				new_lis.append(elem[:-2])
+		return dic,new_lis
 
 gg = initParser("test.txt")
 lis = gg.preprocess_file()
-gg.write_to_file("testWrite.txt",lis)
+dic,no_label_list = gg.generate_labels_and_list("test.txt")
+print(dic)
+gg.write_to_file("testWrite.txt",no_label_list)
