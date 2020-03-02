4af72ea9acd6eb0b6e8688815aca99e66bb7a362
RishabhAgarwal-2001 <rishabh2001agarwal@gmail.com>
2020-03-02 16:04:13 +0530

    All Classes except U-format Completed

--- lib/instructionClass.py
+++ lib/instructionClass.py
@@ -1,51 +1,69 @@
 table = [['addi', '#################000#####0010011', 'I']]
 
+def numberToBinary(self, number, bits):
+    binaryString = ""
+    while(number>0):
+        binaryString = binaryString + str(number%2)
+        number = number//2
+    binaryString = binaryString[::-1]
+    if(len(binaryString)<bits):
+        extraLength = bits - len(binaryString)
+        zeros = '0'*extraLength
+        binaryString = zeros + binaryString
+    return binaryString
 
-# To be Completed By Shobhit
 class R:
-    pass
+    def machineCode(self, instr, destReg, srcReg1, srcReg2):
+        for i in table:
+            if(i[0] != instr):
+                continue
+            else:
+                mCode = i[1][:8] + numberToBinary(srcReg2, 5) + numberToBinary(srcReg1, 5) + i[1][17:20] + numberToBinary(destReg, 5) + i[1][25:]
+                return mCode
+        return ""
 
-# To be Completed By Shobhit
+
 class S:
-    pass
+    def machineCode(self, instr, immediate, func3, srcReg1, srcReg2):
+        for i in table:
+            if(i[0] != instr):
+                continue
+            else:
+                mCode = numberToBinary(immediate, 12)[:8] + numberToBinary(
+                    srcReg2, 5) + numberToBinary(srcReg1, 5) + i[1][17:20] + numberToBinary(immediate, 12)[8:] + i[1][25:]
+                return mCode
+        return ""
 
-# To be Completed By Rishabh
 class I:
-    opcode = rd = func3 = rs1 = immediate = ""
-    def numberToBinary(self, number, bits):
-        binaryString = ""
-        while(number>0):
-            binaryString = binaryString + str(number%2)
-            number = number//2
-        binaryString = binaryString[::-1]
-        if(len(binaryString)<bits):
-            extraLength = bits - len(binaryString)
-            zeros = '0'*extraLength
-            binaryString = zeros + binaryString
-        return binaryString
-
     def machineCode(self, instr, destReg, srcReg, immediate):
         for i in table:
             if(i[0]!=instr):
                 continue
             else:
-                mCode = self.numberToBinary(immediate, 12) + self.numberToBinary(srcReg, 5) + i[1][17:20] + self.numberToBinary(destReg, 5) + i[1][25:]
+                mCode = numberToBinary(immediate, 12) + numberToBinary(srcReg, 5) + i[1][17:20] + numberToBinary(destReg, 5) + i[1][25:]
+                return mCode
+        return ""
+class UJ:
+    def machineCode(self, instr, destReg, immediate):
+        for i in table:
+            if(i[0]!=instr):
+                continue
+            else:
+                mCode = numberToBinary(immediate, 21)[
+                    0] + numberToBinary(immediate, 21)[10:20] + numberToBinary(immediate, 21)[9] + numberToBinary(immediate, 21)[1:9] + numberToBinary(destReg, 5) + i[1][25:]
                 return mCode
         return ""
 
-# To be Completed By Shobhit
-class UJ:
-    pass
-# To be Completed By Rishabh
-class SB:
-    pass
 
+class SB:
+    def machineCode(self, instr, immediate, srcReg1, srcReg2):
+        for i in table:
+            if(i[0]!=instr):
+                continue
+            else:
+                mCode = numberToBinary(immediate, 13)[0] + numberToBinary(immediate, 13)[2:8] + numberToBinary(srcReg2, 5) + numberToBinary(srcReg1, 5) + i[1][17:20] + numberToBinary(immediate, 13)[8:12] + numberToBinary(immediate, 13)[1] + i[1][25:]
+                return mCode
 
 # To be Completed By Rishabh
 class U:
-    pass
-
-
-i_object = I()
-machineCode = i_object.machineCode("addi", 5, 6, 8)
-print(machineCode)
+    def machineCode(self, instr, )
