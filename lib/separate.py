aebe7e964e199aacbbbac3e1105ac50988d9b203
shobhit <shobhitgupta907@gmail.com>
2020-03-02 16:58:33 +0530

    Done

--- lib/separate.py
+++ lib/separate.py
@@ -6,16 +6,19 @@ dataWrite=False
 textWrite=False
 
 for line in input_file.readlines():
+    if(line.strip()==".data"):
+        dataWrite=True
+        textWrite=False
+        continue
+    elif(line.strip()==".text"):
+        dataWrite=False
+        textWrite=True
+        continue
     if dataWrite:
         data_file.write(line)
     if textWrite:
         text_file.write(line)
-    if(line.strip()==".data"):
-        dataWrite=True
-        textWrite=False
-    elif(line.strip()==".text"):
-        dataWrite=False
-        textWrite=True
+    
 
 input_file.close()
 data_file.close()
