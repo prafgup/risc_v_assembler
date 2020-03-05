b102fc8b2097569f359d4e35826c6784ffbb38d0
Shubham <shubhamsingh14272000@gmail.com>
2020-03-06 00:10:12 +0530

    mc_gen file compile error corrected

--- lib/mc_gen.py
+++ lib/mc_gen.py
@@ -8,7 +8,6 @@ def main1():
     lineNo = 0
     for instruction in allInstructions:
         lineNo=lineNo+1
-        i
         if (instruction[-1] == '\n'):
             instruction = instruction[0:-1]
         instructionParts = instruction.split(' ')
