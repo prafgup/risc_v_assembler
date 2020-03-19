2fe854ac3a9ae201ebff80ec8400100dd92714cf
Praful Gupta <prafulgupta6@gmail.com>
2020-03-19 18:57:36 +0530

    phase 1 working

--- lib/Files/assemblyCodeProcessed.asm
+++ lib/Files/assemblyCodeProcessed.asm
@@ -2,6 +2,12 @@
 name: .asciiz "Rishabh Agarwal"
 .text
 addi x5 x0 1
+addi x5 x0 1
+addi x5 x0 1
+addi x5 x0 1
+addi x5 x0 1
+addi x5 x0 1
+addi x5 x0 1
 add x5 x6 x5
 label1:
 beq x0 x0 label2
--- lib/Files/assemblyCodeText.asm
+++ lib/Files/assemblyCodeText.asm
@@ -1,4 +1,10 @@
 addi x5 x0 1
+addi x5 x0 1
+addi x5 x0 1
+addi x5 x0 1
+addi x5 x0 1
+addi x5 x0 1
+addi x5 x0 1
 add x5 x6 x5
 label1:
 beq x0 x0 label2
