f1c8899d4084599ef3381148a72fe1ff6ff2baa4
RishabhAgarwal-2001 <rishabh2001agarwal@gmail.com>
2020-03-23 22:48:56 +0530

    .

--- lib/Files/assemblyCode.asm
+++ lib/Files/assemblyCode.asm
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-.data
-rishabh: .word 56
-.text
-lw x3 rishabh
-=======
 .text
 #beq x0 x0 label
 #beq x0 x0 label2
@@ -17,4 +11,3 @@ lw x3 rishabh
 
 addi x1 x0 5
 sb x1 -12(sp)
->>>>>>> 7fec26edb921f6da66ad58ca2f888d08d32f6dd9
