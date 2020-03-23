cc224d9ed7bc6f9d6dfc8f919f9a69f7b8cc673b
RishabhAgarwal-2001 <rishabh2001agarwal@gmail.com>
2020-03-23 13:16:25 +0530

    Code To Obtain Machine Code For Phase 2

---
+++ lib/Phase2/getMC.py
@@ -0,0 +1,29 @@
+import os
+
+
+'''
+    Code To Fetch Machine Code written in Hexadecimal From Phase 1 Output File 
+    The instructions are then converted to binary
+    Instruction Address is Removed
+    The Final Compiled Machine Code is saved in the Files Section of Phase 2 
+    The name of The final file is machineCode.mc
+'''
+def getMachineCode():
+    currentWorkingDirectory = os.getcwd()
+    pathToOriginalFile = currentWorkingDirectory + "/../Files/machine_code.mc"
+    pathToCompiledFile = currentWorkingDirectory + "/Files/machineCode.mc"
+    pointerOF = open(pathToOriginalFile, "r")
+    pointerCF = open(pathToCompiledFile, "w+")
+    dataOriginalFile = pointerOF.readlines()
+    for line in dataOriginalFile:
+        parts = line.split(" ")
+        instructionH = parts[1]
+        instructionH = instructionH.strip()
+        instructionD = int(instructionH, 16)
+        instructionB = bin(instructionD).replace("0b", "")
+        instructionB = instructionB.zfill(32)
+        pointerCF.write(instructionB + "\n")
+    pointerCF.close()
+    pointerOF.close()
+
+getMachineCode()
