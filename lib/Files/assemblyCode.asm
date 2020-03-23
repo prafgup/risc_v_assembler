5c57cd8c0291972651fecedf137d0d1947fcf627
Shubham <shubhamsingh14272000@gmail.com>
2020-03-23 19:35:55 +0530

    stack pointer working

--- lib/Files/assemblyCode.asm
+++ lib/Files/assemblyCode.asm
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-.data
-lol: .word 10
-
 .text
 #beq x0 x0 label
 #beq x0 x0 label2
@@ -14,13 +9,5 @@ lol: .word 10
 #addi x0 x0 1
 #end:
 
-<<<<<<< HEAD
 addi x1 x0 5
 sb x1 -12(sp)
-=======
-lui x5 65536
-addi x4 x0 4
-add x7 x5 x4
-sw x4 0(x7)
->>>>>>> 8d31714509555dfcf8852a979dc15ab2e890cce6
->>>>>>> 5186feeef08a90ecbc5805b723e3f9c97e73256e
