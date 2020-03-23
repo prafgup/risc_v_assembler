66c934530f46f5a14addc053e5a2e85426081961
RishabhAgarwal-2001 <rishabh2001agarwal@gmail.com>
2020-03-23 19:15:18 +0530

    Memory Assembly Code

--- lib/Files/assemblyCode.asm
+++ lib/Files/assemblyCode.asm
@@ -1,18 +1,4 @@
 .data
-lol: .word 10
-
+rishabh: .word 56
 .text
-#beq x0 x0 label
-#beq x0 x0 label2
-#beq x0 x0 label
-#beq x0 x0 label
-#label:
-#addi x0 x0 1
-#label2:
-#addi x0 x0 1
-#end:
-
-lui x5 65536
-addi x4 x0 4
-add x7 x5 x4
-sw x4 0(x7)
+lw x3 rishabh
