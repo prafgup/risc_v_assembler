b5434f5c29f7c1eff580b758c1d1ff622ac065b2
Praful Gupta <prafulgupta6@gmail.com>
2020-03-23 14:41:37 +0530

    label offste fix +ve and +ve

--- lib/Files/assemblyCodeFinal.asm
+++ lib/Files/assemblyCodeFinal.asm
@@ -3,12 +3,17 @@ addi x5 x0 1
 addi x5 x0 1
 addi x5 x0 1
 addi x5 x0 1
+beq x0 x0 60
+beq x0 x0 20
 addi x5 x0 1
 addi x5 x0 1
 add x5 x6 x5
 beq x0 x0 0
 addi x6 x6 10
-beq x0 x0 16
+beq x0 x0 -4
+beq x0 x0 28
 lw x1 rishabh
-beq x0 x0 -12
+beq x0 x0 -20
 addi x5 x5 1
+beq x0 x0 -28
+beq x0 x0 -32
