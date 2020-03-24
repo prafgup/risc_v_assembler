62d0784e06d8e78c9b3265d436cba2253309ed2b
Shubham <shubhamsingh14272000@gmail.com>
2020-03-24 18:15:03 +0530

    ab

--- lib/Files/assemblyCode.asm
+++ lib/Files/assemblyCode.asm
@@ -1,6 +1,19 @@
+.data
+var1: .word -5
+var2: .word -10
+
 .text
-addi x5 x0 2
-addi x4 x0 5
-lui x3 65536
-sw x5 0(x3)
-sw x4 4(x3)
+addi x5,x0,10
+addi x6,x0,0
+lui x7,65536
+addi x9,x0,5
+loop1:
+lb x8,0(x7)
+addi x10,x0,0
+slt x10,x8,x9
+addi x7,x7,4
+addi x5,x5,-1
+bne x10,x0,adds
+add x6,x6,x8
+adds:
+bne x5,x0,loop1
--- lib/Files/assemblyCodeText.asm
+++ lib/Files/assemblyCodeText.asm
@@ -1,5 +1,6 @@
-addi x5 x0 2
-addi x4 x0 5
+addi x5 x0 -2
+addi x4 x0 -50
 lui x3 65536
 sw x5 x3 0
 sw x4 x3 4
+lw x8 x3 4
