436e8c1dd60e8392e8a20de7017479375d292b02
RishabhAgarwal-2001 <rishabh2001agarwal@gmail.com>
2020-03-24 22:32:36 +0530

    Test and ReadMe updated

--- lib/Files/assemblyCodeFinal.asm
+++ lib/Files/assemblyCodeFinal.asm
@@ -1,1 +1,12 @@
-addi x5 x0 3
+addi x5 x0 10
+addi x6 x0 0
+lui x7 65536
+addi x9 x0 5
+lb x8 x7 0
+addi x10 x0 0
+slt x10 x8 x9
+addi x7 x7 4
+addi x5 x5 -1
+bne x10 x0 8
+add x6 x6 x8
+bne x5 x0 -28
--- lib/Files/assemblyCodeText.asm
+++ lib/Files/assemblyCodeText.asm
@@ -1,1 +1,14 @@
-addi x5 x0 3
+addi x5 x0 10
+addi x6 x0 0
+lui x7 65536
+addi x9 x0 5
+loop1:
+lb x8 x7 0
+addi x10 x0 0
+slt x10 x8 x9
+addi x7 x7 4
+addi x5 x5 -1
+bne x10 x0 adds
+add x6 x6 x8
+adds:
+bne x5 x0 loop1
